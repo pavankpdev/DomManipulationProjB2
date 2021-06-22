e28d75ace92cea424a2fd87cc65157bfdaae76f2
pavanleton <pavankumarkpkp@gmail.com>
2021-06-22 21:15:21 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = []; // some values []
+let globalStore = []; // some values []
 
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4">
 <div class="card">
   <div class="card-header d-flex justify-content-end gap-2">
     <button type="button" class="btn btn-outline-success">
       <i class="fas fa-pencil-alt"></i>
     </button>
-    <button type="button" class="btn btn-outline-danger">
-      <i class="fas fa-trash-alt"></i>
+    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)">
+      <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this, arguments)"></i>
     </button>
   </div>
   <img
@@ -42,16 +42,15 @@ const loadInitialCardData = () => {
   // convert from string to normal object
   const {cards} = JSON.parse(getCardData);
 
-  // loop over those array of task object to create HTML card, inject it to DOM
+  // loop over those array of task object to create HTML card, 
   cards.map((cardObject) => {
 
+    // inject it to DOM
     taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
-    
+
     // update our globalStore
     globalStore.push(cardObject);
   })
-
-
  
 };
 
@@ -73,14 +72,35 @@ const saveChanges = () => {
 
 };
 
+const deleteCard = (event) => {
+  event = window.event;
+  // id
+  const targetID = event.target.id;
+  const tagname = event.target.tagName; // BUTTON
+  // match the id of the element with the id inside the globalStore
+  // if match found remove
+
+  globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID); 
+  localStorage.setItem("tasky", JSON.stringify({cards:globalStore})); // an object
+  // contact parent
+
+  if(tagname === "BUTTON"){
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+  }else{
+    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+  }
+
+};
 
 // Issues
 
-// Page refresh will cause the data to be deleted -> localstorage -> 5MB
+// Page refresh will cause the data to be deleted -> localstorage -> 5MB [solved]
 
 
 
 // Features
-// Delete The card
+
+// Delete The card  [solved]
+
 // Edit the card
 // Open the card
